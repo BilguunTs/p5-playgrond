8ecd368136b226a5e62489edb61ff09dbb916098
BilguunTs <usosketch@yahoo.com>
2021-06-26 19:50:07 +0800

    ndd

---
+++ js/Particle.js
@@ -0,0 +1,28 @@
+class Particle {
+  constructor() {
+    this.pos = createVector(width / 2, height / 2);
+    this.rays = [];
+    for (let a = 0; a < 360; a += 5) {
+      this.rays.push(new Ray(this.pos, radians(a)));
+    }
+  }
+  look(wall) {
+    for (let ray of this.rays) {
+      const pt = ray.cast(wall);
+      if (pt) {
+        line(this.pos.x, this.pos.y, pt.x, pt.y);
+      }
+    }
+  }
+
+  show() {
+    fill(255);
+    circle(this.pos.x, this.pos.y, 16);
+    for (let ray of this.rays) {
+      ray.show();
+    }
+  }
+  update(x, y) {
+    this.pos.set(x, y);
+  }
+}
--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 let wall;
 let ray;
+let particle;
 function setup() {
   createCanvas(windowWidth * 0.8, windowHeight * 0.9);
   wall = new Boundary(400, 100, 400, 400);
-  ray = new Ray(200, 200);
+  particle = new Particle();
 }
 
 function draw() {
   background(10);
   wall.show();
-  ray.show();
+
+  particle.update(mouseX, mouseY);
+  particle.show();
+  particle.look(wall);
+  // let pt = ray.cast(wall);
+  // if (pt) {
+  //   fill(255);
+  //   circle(pt.x, pt.y, 20);
+  // }
 }
